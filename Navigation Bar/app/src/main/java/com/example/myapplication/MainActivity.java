29e033be053c6f3269158dca431b5e62b695f697
Your Name <you@example.com>
2021-01-21 19:38:21 +0000

    updated navigation bar using navigationlistener

--- Navigation Bar/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Navigation Bar/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = findViewById(R.id.nav_id);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.email:
+                        Intent profileIntent = new Intent(MainActivity.this, emailTest.class);
+                        startActivity(profileIntent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
